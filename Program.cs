bcd209d689a322172df701c3db08346d0858d74b
David Brockwell <dbrockwell@my.wctc.edu>
2022-02-28 13:49:33 -0600

    created menu with choice

--- Program.cs
+++ Program.cs
@@ -10,10 +10,25 @@ namespace MediaLibrary
         static void Main(string[] args)
         {
             logger.Info("Program started");
-
             string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
             logger.Info(scrubbedFile);
             MovieFile movieFile = new MovieFile(scrubbedFile);
+
+            string choice = "";
+            do{
+                Console.WriteLine("1) Add Movie");
+                Console.WriteLine("2) Display All Movies");
+                Console.WriteLine("Enter to quit");
+                choice = Console.ReadLine();
+                logger.Info("User choice: {Choice}", choice);
+
+                if(choice == "1") {
+
+                } else if(choice == "2") {
+                    
+                }
+            } while (choice == "1" || choice == "2");
+
 
             logger.Info("Program ended");
         }
